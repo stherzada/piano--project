46c741462631f4310ad7fef7ac68942af626a772
Caio Gomes <ocaioluis@gmail.com>
2024-01-26 00:24:46 +0000

    wip: feat: notes playing in the right order log a youtube link with its music xD
    Signed-off-by: Caio Gomes <ocaioluis@gmail.com>

--- src/components/Key/Key.tsx
+++ src/components/Key/Key.tsx
@@ -1,40 +1,49 @@
-import React, { FunctionComponent, ReactEventHandler } from "react"
-import clsx from "clsx"
-import { NoteType } from "../../domain/note"
-import { usePressObserver } from "../PressObserver"
-import "./style.css"
+import React, { FunctionComponent, ReactEventHandler, useEffect } from "react";
+import clsx from "clsx";
+import { NoteType } from "../../domain/note";
+import { usePressObserver } from "../PressObserver";
+import "./style.css";
 
 interface KeyProps {
-  type: NoteType
-  label: string
-  disabled?: boolean
+    type: NoteType;
+    label: string;
+    disabled?: boolean;
 
-  onUp: ReactEventHandler<HTMLButtonElement>
-  onDown: ReactEventHandler<HTMLButtonElement>
+    onUp: ReactEventHandler<HTMLButtonElement>;
+    onDown: ReactEventHandler<HTMLButtonElement>;
+    sequence: string;
+    setSequence: React.Dispatch<React.SetStateAction<string>>;
 }
 
 export const Key: FunctionComponent<KeyProps> = ({
-  type,
-  label,
-  onDown,
-  onUp,
-  ...rest
+    type,
+    label,
+    onDown,
+    onUp,
+    sequence,
+    setSequence,
+    ...rest
 }) => {
-  const pressed = usePressObserver({
-    watchKey: label,
-    onStartPress: onDown,
-    onFinishPress: onUp
-  })
+    const pressed = usePressObserver({
+        watchKey: label,
+        onStartPress: onDown,
+        onFinishPress: onUp,
+    });
 
-  return (
-    <button
-      className={clsx(`key key--${type}`, pressed && "is-pressed")}
-      onMouseDown={onDown}
-      onMouseUp={onUp}
-      type="button"
-      {...rest}
-    >
-      {label}
-    </button>
-  )
-}
+    useEffect(() => {
+        if (!pressed) return;
+        setSequence(sequence + label);
+    }, [pressed, label]);
+
+    return (
+        <button
+            className={clsx(`key key--${type}`, pressed && "is-pressed")}
+            onMouseDown={onDown}
+            onMouseUp={onUp}
+            type="button"
+            {...rest}
+        >
+            {label}
+        </button>
+    );
+};
