39af84947b0e315b7370487f71cc34208eed1679
Sther <sthefanypequeno@gmail.com>
2024-01-27 20:43:55 -0300

    feat: creating a modal.

--- src/components/Playground/Playground.tsx
+++ src/components/Playground/Playground.tsx
@@ -7,20 +7,30 @@ import "./style.css";
 export const Playground = () => {
     const [unlockedMusic, setUnlockedMusic] = useState<infoMusics>();
 
-    useEffect(() => {}, [unlockedMusic]);
+    useEffect(() => { }, [unlockedMusic]);
 
     return (
         <div className="container">
             <InstrumentContextProvider>
                 {unlockedMusic && (
-                 <>   
-                 <button onClick={() => setUnlockedMusic(undefined)}> X </button>
-                 <p>{unlockedMusic.author}</p>
-                 <a href={unlockedMusic.url} className="secret">
-                 Você desbloqueou uma música, clique aqui para ouvi-la
-             </a>
-             </>
-                 
+                    <>
+                        <div className="unlocked">
+                            <div className="unlocked-music">
+                                <span className="close"> <button onClick={() => setUnlockedMusic(undefined)}> <img width="30" height="30" src="https://img.icons8.com/ios-filled/50/x.png" alt="x"/> </button></span>
+                                <div className="music">
+                                <h2>{unlockedMusic.music}</h2>
+                                <p>{unlockedMusic.author}</p>
+                                </div>
+                                <span className="describle">
+                                    <p>{unlockedMusic.describle}</p>
+                                </span>
+                                <a href={unlockedMusic.url} className="secret">
+                                    Você desbloqueou uma música, clique aqui para ouvi-la
+                                </a>
+                            </div>
+                        </div>
+                    </>
+
                 )}
                 <div className="playground">
                     <KeyboardWithInstrument setMessage={setUnlockedMusic} />
