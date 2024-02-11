f805ad824eca41478b263ba3e8722a1cc7a82ec8
Sther <sthefanypequeno@gmail.com>
2024-02-11 18:20:57 -0300

    style: creating close button and link button

--- src/components/Playground/Playground.tsx
+++ src/components/Playground/Playground.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { InstrumentContextProvider } from "../../state/Instrument";
 import { InstrumentSelector } from "../InstrumentSelector";
 import { KeyboardWithInstrument, infoMusics } from "../Keyboard";
-import "./style.css";
+import "./style.scss";
 
 export const Playground = () => {
     const [unlockedMusic, setUnlockedMusic] = useState<infoMusics>();
@@ -14,19 +14,31 @@ export const Playground = () => {
             <InstrumentContextProvider>
                 {unlockedMusic && (
                     <>
+                        
                         <div className="unlocked">
                             <div className="unlocked-music">
-                                <span className="close"> <button onClick={() => setUnlockedMusic(undefined)}> <img width="30" height="30" src="https://img.icons8.com/ios-filled/50/x.png" alt="x"/> </button></span>
+                                <div className="wrapper">
+                                    <a onClick={() => setUnlockedMusic(undefined)} className="close-button">
+                                        <div className="in">
+                                            <div className="close-button-block"></div>
+                                            <div className="close-button-block"></div>
+                                        </div>
+                                        <div className="out">
+                                            <div className="close-button-block"></div>
+                                            <div className="close-button-block"></div>
+                                        </div>
+                                    </a>
+                                </div>
                                 <div className="music">
-                                <h1>{unlockedMusic.music}</h1>
-                                <p>{unlockedMusic.author}</p>
+                                    <h1>{unlockedMusic.music}</h1>
+                                    <p>{unlockedMusic.author}</p>
                                 </div>
                                 <span className="describle">
                                     <p>{unlockedMusic.describle}</p>
                                 </span>
-                                <a href={unlockedMusic.url} className="secret" target="blank">
-                                    Clique aqui para ouvir a música ✨
-                                </a>
+
+                                <a href={unlockedMusic.url} target="_blank" className="secret">Clique aqui para ouvir a música ✨</a>
+
                             </div>
                         </div>
                     </>
